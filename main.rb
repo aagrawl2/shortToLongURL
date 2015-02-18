b3c9743e893c211fd0f2c957eff49ab272fcd7d5
Akshay Agrawal <akshay.agrawal1101@gmail.com>
2015-02-18 14:39:26 -0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
+#Lines 2- 13 is required if you want to install dependencies in the run time
 require "json"
 require 'bundler/cli'
-#
  
 require 'rubygems'
 require 'bundler'
 require 'net/http'
 
-
+#Initialize bundler to handle run time dependencies
 bundle = Bundler::CLI.new
 bundle.invoke(:install,[],:path => './',:verbose => false, :deployment => false)
 Bundler.setup(:default, :ci)
@@ -16,13 +16,13 @@ class Link
  
   def initialize(message_link)
     @short = message_link
-    @client = Mechanize.new
+    @client = Mechanize.new #Create new mechanize object
   end
  
   # Opens the shortened url, grabs redirect header.
   def long_url
     @client.redirect_ok = false
-    page = @client.get @short
+    page = @client.get @short #Get the Long URL
     long =  page.header['location']
     puts "#{@short} => #{long}"
     long
